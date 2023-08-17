06629f9432c37be8abf68525d29e78c2b53eefec
Wittchen <swittche@ipb-halle.de>
2023-08-17 14:21:52 +0200

    first commit

---
+++ src/main/java/org/example/Launcher.java
@@ -0,0 +1,16 @@
+package org.example;
+
+import org.apache.commons.lang3.SystemUtils;
+
+public class Launcher {
+    public static void main(String[] args) {
+//        System.out.println(SystemUtils.getJavaHome());
+
+        print();
+    }
+
+    private static void print() {
+        System.out.println("Hello World!");
+    }
+
+}
